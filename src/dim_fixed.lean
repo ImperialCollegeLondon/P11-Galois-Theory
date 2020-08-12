1afc5020da9a59fc7b82d33378c93255b7df0104
kckennylau <kc_kennylau@yahoo.com.hk>
2020-08-13 01:18:13 +0800

    done

--- src/dim_fixed.lean
+++ src/dim_fixed.lean
@@ -1,72 +1,45 @@
 import linear_algebra.dimension
 import field_theory.fixed
+import field_theory.tower
 import .faithful_mul_semiring_action .dimension .linear_independent .group_ring_action .finsupp
 
 universes u v
 
-open_locale classical
-open mul_action
+open_locale classical big_operators
+open mul_action finset
 
-variables (G : Type u) [group G] (F : Type v) [field F] [mul_semiring_action G F] (g : G)
+variables (G : Type u) [group G] (F : Type v) [field F] [mul_semiring_action G F]
 
--- set_option pp.all true
-theorem mem_fixed {s : finset F} {a : F} (has : a ∉ s)
-  (h1 : linear_independent F (λ i : (↑(s.map $ mul_action.to_fun G F) : set (G → F)), (i : G → F)))
-  {L : F →₀ F} (h2 : L ∈ finsupp.supported F F (↑s : set F))
-  (h3 : mul_action.to_fun G F a = s.sum (λ i, L i • mul_action.to_fun G F i)) {i : F} :
-  L i ∈ fixed_points G F :=
-begin
-  intro g,
-  by_cases hi : i ∈ s,
-  { rw linear_independent_iff at h1,
-    specialize h1 (s.attach.sum $ λ i, finsupp.single ⟨_, finset.mem_map_of_mem _ i.2⟩ (g • L i.1 - L i.1)) _,
-    { rw finsupp.ext_iff at h1, specialize h1 ⟨_, finset.mem_map_of_mem _ hi⟩,
-      rwa [finset.sum_apply', finset.sum_eq_single (⟨i, hi⟩ : {x // x ∈ s}), finsupp.single_eq_same,
-          finsupp.zero_apply, sub_eq_zero] at h1,
-      { exact λ b _ hb, finsupp.single_eq_of_ne (λ H, hb $ subtype.eq $ (mul_action.to_fun G F).2 $
-          by convert congr_arg subtype.val H) },
-      { exact λ h, absurd (finset.mem_attach _ _) h } },
-    { ext g',
-      rw [finsupp.total_apply],
-      erw finsupp.sum_apply'.{(max u v) v u v}
-          (s.attach.sum $ λ i, finsupp.single (⟨_, finset.mem_map_of_mem _ i.2⟩ : (↑(finset.map (mul_action.to_fun G F) s) : set (G → F))) (g • L i.1 - L i.1))
-          (λ i (a : F), (a • ↑i : G → F))
-          g',
-      simp_rw pi.smul_apply,
-      rw finsupp.sum_sum_index', iterate 3 { sorry } } },
-  sorry
-  -- intro g,
-/-   have h3 := congr_fun h2 1,
-  have h4 := congr_fun h2 g,
-  simp_rw [finset.sum_apply, pi.smul_apply, mul_action.to_fun_apply, smul_eq_mul] at h3 h4,
-  simp_rw one_smul at h3,
-  rw [h3, finset.smul_sum, ← sub_eq_zero, ← finset.sum_sub_distrib] at h4,
- -/end
-
-#check linear_independent_iff
-
-
--- #exit
 theorem linear_independent_smul_of_linear_independent {s : finset F} :
   linear_independent (fixed_points G F) (λ i : (↑s : set F), (i : F)) →
   linear_independent F (λ i : (↑s : set F), mul_action.to_fun G F i) :=
 begin
   refine finset.induction_on s (λ _, linear_independent_empty_type $ λ ⟨x⟩, x.2) (λ a s has ih hs, _),
-  rw finset.coe_insert at hs ⊢,
-  rw linear_independent_insert (mt finset.mem_coe.1 has) at hs,
-  rw linear_independent_insert' (mt finset.mem_coe.1 has),
-  sorry
-  -- refine (ih hs.1).insert _,
-  -- rw [finset.map_insert, finset.coe_insert],
-  -- rw [finset.coe_insert, linear_independent_insert (mt finset.mem_coe.1 has)] at hs,
-  -- refine (ih hs.1).insert (λ ha, hs.2 _),
-  -- rw [finset.coe_map, finsupp.mem_span_iff_total] at ha,
-  -- rcases ha with ⟨L, hL, ha⟩,
-  -- rw [← set.image_id ↑s, finsupp.mem_span_iff_total],
-  -- rw [finsupp.total_apply_of_mem_supported hL] at ha,
+  rw coe_insert at hs ⊢,
+  rw linear_independent_insert (mt mem_coe.1 has) at hs,
+  rw linear_independent_insert' (mt mem_coe.1 has), refine ⟨ih hs.1, λ ha, _⟩,
+  rw finsupp.mem_span_iff_total at ha, rcases ha with ⟨l, hl, hla⟩,
+  rw [finsupp.total_apply_of_mem_supported hl] at hla,
+  suffices : ∀ i ∈ s, l i ∈ fixed_points G F,
+  { replace hla := (sum_apply _ _ (λ i, l i • to_fun G F i)).symm.trans (congr_fun hla 1),
+    simp_rw [pi.smul_apply, to_fun_apply, one_smul] at hla,
+    refine hs.2 (hla ▸ submodule.sum_mem _ (λ c hcs, _)),
+    change (⟨l c, this c hcs⟩ : fixed_points G F) • c ∈ _,
+    exact submodule.smul_mem _ _ (submodule.subset_span $ mem_coe.2 hcs) },
+  intros i his g,
+  refine eq_of_sub_eq_zero (linear_independent_iff'.1 (ih hs.1) s.attach (λ i, g • l i - l i) _
+    ⟨i, his⟩ (mem_attach _ _) : _),
+  refine (@sum_attach _ _ s _ (λ i, (g • l i - l i) • (to_fun G F) i)).trans _, ext g', dsimp only,
+  conv_lhs { rw sum_apply, congr, skip, funext, rw [pi.smul_apply, sub_smul, smul_eq_mul] },
+  rw [sum_sub_distrib, pi.zero_apply, sub_eq_zero],
+  conv_lhs { congr, skip, funext,
+    rw [to_fun_apply, ← mul_inv_cancel_left g g', mul_smul, ← smul_mul', ← to_fun_apply _ x] },
+  show ∑ x in s, g • (λ y, l y • to_fun G F y) x (g⁻¹ * g') =
+    ∑ x in s, (λ y, l y • to_fun G F y) x g',
+  rw [← smul_sum, ← sum_apply _ _ (λ y, l y • to_fun G F y),
+      ← sum_apply _ _ (λ y, l y • to_fun G F y)], dsimp only,
+  rw [hla, to_fun_apply, to_fun_apply, smul_smul, mul_inv_cancel_left]
 end
-
-#check nonempty
 
 variables [fintype G]
 
@@ -77,20 +50,25 @@ begin
     cardinal.finset_card, ← cardinal.lift_id (vector_space.dim F (G → F))],
   refine le_dim.{_ _ _ (max u v)} (λ i : (↑s : set F), mul_action.to_fun G F i.1) _,
   exact linear_independent_smul_of_linear_independent G F hs
-/-   refine finset.induction_on s (λ _, linear_independent_empty) (λ a s has ih hs, _),
-  rw [finset.map_insert, finset.coe_insert],
-  rw [finset.coe_insert, linear_independent_insert (mt finset.mem_coe.1 has)] at hs,
-  refine (ih hs.1).insert (λ ha, hs.2 _),
-  rw [finset.coe_map, function.embedding.coe_fn_mk, finsupp.mem_span_iff_total] at ha,
-  rcases ha with ⟨L, hL, ha⟩,
-  rw [← set.image_id ↑s, finsupp.mem_span_iff_total],
-  rw [finsupp.total_apply_of_mem_supported hL] at ha,
-  suffices : ∀ x, ∀ g : G, g • L x = L x,
-  { refine ⟨s.sum $ λ i, finsupp.single i ⟨L i, λ g, this i g⟩,
-      submodule.sum_mem _ (λ i his, finsupp.single_mem_supported _ _ his), _⟩,
-      }, -/
 end
 
-#check finsupp.sum_single
-#check finsupp.total_apply
-#check cardinal.lift_le
+#check linear_independent_monoid_hom
+/-
+G → (F →ₐ[fixed_points G F] F)
+g ↦ (x ↦ g • x)
+
+lindep F (coe : (F →ₐ[fixed_points G F] F) → (F →ₗ[fixed_points G F] F))
+
+Hom(V, W) <-- V* ⊗ W
+dim L (V ⊗[K] L) = dim K V
+
+When in doubt, pick a basis
+
+    dim F (F →ₗ[fixed_points G F] F)
+"=" dim F ((F →ₗ[fixed_points G F] (fixed_points G F)) ⊗[fixed_points G F] F)
+"=" dim (fixed_points G F) (F →ₗ[fixed_points G F] (fixed_points G F))
+"=" dim (fixed_points G F) F
+-/
+
+open finite_dimensional
+#check findim_mul_findim (fixed_points G F) F (F →ₗ[fixed_points G F] F) -- F ⊆ K ⊆ A

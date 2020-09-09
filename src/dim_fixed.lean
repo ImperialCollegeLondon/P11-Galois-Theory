33855ab92caafbc19213ea912593ddd23bae3724
kckennylau <kc_kennylau@yahoo.com.hk>
2020-09-10 01:45:50 +0800

    finally prove reverse galois theory

--- src/dim_fixed.lean
+++ src/dim_fixed.lean
@@ -7,11 +7,11 @@ import .faithful_mul_semiring_action .dimension .linear_independent .group_ring_
 universes u v w
 
 open_locale classical big_operators
-open mul_action finset
+open mul_action finset finite_dimensional
 
 variables (G : Type u) [group G] (F : Type v) [field F] [mul_semiring_action G F]
 
-theorem linear_independent_smul_of_linear_independent {s : finset F} :
+lemma linear_independent_smul_of_linear_independent {s : finset F} :
   linear_independent (fixed_points G F) (λ i : (↑s : set F), (i : F)) →
   linear_independent F (λ i : (↑s : set F), mul_action.to_fun G F i) :=
 begin
@@ -44,7 +44,7 @@ end
 
 variables [fintype G]
 
-theorem fixed.dim_le_card : vector_space.dim (fixed_points G F) F ≤ fintype.card G :=
+lemma fixed_points.dim_le_card : vector_space.dim (fixed_points G F) F ≤ fintype.card G :=
 begin
   refine dim_le (λ s hs, cardinal.nat_cast_le.1 _),
   rw [← @dim_fun' F G, ← cardinal.lift_nat_cast.{v (max u v)},
@@ -52,6 +52,13 @@ begin
   refine le_dim.{_ _ _ (max u v)} (λ i : (↑s : set F), mul_action.to_fun G F i.1) _,
   exact linear_independent_smul_of_linear_independent G F hs
 end
+
+instance finite_dimensional_fixed_points : finite_dimensional (fixed_points G F) F :=
+finite_dimensional.finite_dimensional_iff_dim_lt_omega.2 $
+lt_of_le_of_lt (fixed_points.dim_le_card G F) (cardinal.nat_lt_omega _)
+
+lemma findim_fixed_points : findim (fixed_points G F) F ≤ fintype.card G :=
+by exact_mod_cast trans_rel_right (≤) (findim_eq_dim _ _) (fixed_points.dim_le_card G F)
 
 instance linear_map.semimodule' (R : Type u) [comm_semiring R]
   (M : Type v) [add_comm_monoid M] [semimodule R M]
@@ -81,25 +88,12 @@ instance linear_map.ring (R : Type u) [comm_semiring R]
   (S : Type v) [ring S] [algebra R S] : ring (S →ₗ[R] S) :=
 { .. linear_map.semiring R S, .. linear_map.add_comm_group }
 
--- NOT TRUE! NOT TRUE! NOT TRUE! NOT TRUE! NOT TRUE! NOT TRUE! NOT TRUE! NOT TRUE! NOT TRUE! NOT TRUE!
--- instance linear_map.algebra (R : Type u) [comm_semiring R]
---   (S : Type w) [comm_semiring S] [algebra R S] : algebra S (S →ₗ[R] S) :=
--- { to_fun := algebra.lmul R S,
---   map_one' := linear_map.ext one_mul,
---   map_mul' := λ s₁ s₂, linear_map.ext $ mul_assoc s₁ s₂,
---   map_zero' := linear_map.map_zero _,
---   map_add' := linear_map.map_add _,
---   commutes' := λ s f, linear_map.ext $ λ x, _, -- s * f(x) = f(s * x)
---   smul_def' := _,
---   .. linear_map.semimodule' R S S }
--- NOT TRUE! NOT TRUE! NOT TRUE! NOT TRUE! NOT TRUE! NOT TRUE! NOT TRUE! NOT TRUE! NOT TRUE! NOT TRUE!
-
 instance is_scalar_tower.linear_map (R : Type u) (A : Type v) (V : Type w)
   [comm_semiring R] [comm_semiring A] [add_comm_monoid V]
   [semimodule R V] [algebra R A] : is_scalar_tower R A (V →ₗ[R] A) :=
 ⟨λ x y f, linear_map.ext $ λ v, algebra.smul_mul_assoc x y (f v)⟩
 
-theorem finite_dimensional_of_right (F : Type u) (K : Type v) (V : Type w)
+lemma finite_dimensional_of_right (F : Type u) (K : Type v) (V : Type w)
   [field F] [field K] [add_comm_group V] [algebra F K] [vector_space K V] [vector_space F V]
   [is_scalar_tower F K V] [hf : finite_dimensional F V] :
   finite_dimensional K V :=
@@ -116,9 +110,7 @@ let ⟨b, hb⟩ := finite_dimensional.exists_is_basis_finset F V in
 let ⟨c, hc⟩ := finite_dimensional.exists_is_basis_finset F W in
 linear_equiv.finite_dimensional (linear_equiv_matrix hb hc).symm
 
-open finite_dimensional
-
-theorem findim_linear_map (F : Type u) (V : Type v) (W : Type w)
+lemma findim_linear_map (F : Type u) (V : Type v) (W : Type w)
   [field F] [add_comm_group V] [vector_space F V] [add_comm_group W] [vector_space F W]
   [finite_dimensional F V] [finite_dimensional F W] :
   findim F (V →ₗ[F] W) = findim F V * findim F W :=
@@ -137,7 +129,7 @@ finite_dimensional_of_right F _ _
 -- V = ⊕F,
 -- (V →ₗ[F] K) = ((⊕F) →ₗ[F] K) = (⊕ (F →ₗ[F] K)) = (⊕K) is finite over K
 
-theorem findim_linear_map' (F : Type u) [field F] (K : Type v) [field K]
+lemma findim_linear_map' (F : Type u) [field F] (K : Type v) [field K]
   [algebra F K] [finite_dimensional F K] :
   findim K (K →ₗ[F] K) = findim F K :=
 (nat.mul_right_inj $ show 0 < findim F K, from findim_pos).1 $
@@ -154,6 +146,7 @@ have ∑ (i : ι) in s, g i • f (v i) = 0,
   by simp_rw [← f.map_smul, ← f.map_sum, hg, f.map_zero],
 linear_independent_iff'.1 hfv s g this i his
 
+/-- Linearly coerce a linear map to a function. -/
 def linear_map.lto_fun (R : Type u) (M : Type v) (A : Type w)
   [comm_semiring R] [add_comm_monoid M] [semimodule R M] [comm_ring A] [algebra R A] :
   (M →ₗ[R] A) →ₗ[A] (M → A) :=
@@ -170,24 +163,35 @@ from ((linear_independent_monoid_hom A A).comp
   (λ f g hfg, alg_hom.ext $ monoid_hom.ext_iff.1 hfg) : _),
 linear_independent_of_comp _ _ this
 
-/-
-G → (F →ₐ[fixed_points G F] F)
-g ↦ (x ↦ g • x)
+lemma cardinal_mk_alg_hom (K : Type u) (V : Type v)
+  [field K] [field V] [algebra K V] [finite_dimensional K V] :
+  cardinal.mk (V →ₐ[K] V) ≤ findim V (V →ₗ[K] V) :=
+cardinal_mk_le_findim_of_linear_independent $ linear_independent_to_linear_map K V
 
-lindep F (coe : (F →ₐ[fixed_points G F] F) → (F →ₗ[fixed_points G F] F))
+noncomputable instance alg_hom.fintype (K : Type u) (V : Type v)
+  [field K] [field V] [algebra K V] [finite_dimensional K V] :
+  fintype (V →ₐ[K] V) :=
+classical.choice $ cardinal.lt_omega_iff_fintype.1 $
+lt_of_le_of_lt (cardinal_mk_alg_hom K V) (cardinal.nat_lt_omega _)
 
-Hom(V, W) <-- V* ⊗ W
-dim L (V ⊗[K] L) = dim K V
+lemma findim_alg_hom (K : Type u) (V : Type v)
+  [field K] [field V] [algebra K V] [finite_dimensional K V] :
+  fintype.card (V →ₐ[K] V) ≤ findim V (V →ₗ[K] V) :=
+fintype_card_le_findim_of_linear_independent $ linear_independent_to_linear_map K V
 
-When in doubt, pick a basis
+/-- Embedding produced from a faithful action. -/
+def to_alg_hom_over_fixed_points (G : Type u) (F : Type v) [group G] [field F]
+  [faithful_mul_semiring_action G F] : G ↪ (F →ₐ[fixed_points G F] F) :=
+{ to_fun := λ g, { commutes' := λ x, x.2 g,
+    .. mul_semiring_action.to_semiring_hom G F g },
+  inj' := λ g₁ g₂ hg, injective_to_semiring_hom G F $ ring_hom.ext $ λ x, alg_hom.ext_iff.1 hg x, }
 
-    dim F (F →ₗ[fixed_points G F] F)
-"=" dim F ((F →ₗ[fixed_points G F] (fixed_points G F)) ⊗[fixed_points G F] F)
-"=" dim (fixed_points G F) (F →ₗ[fixed_points G F] (fixed_points G F))
-"=" dim (fixed_points G F) F
-
-#G ≤ #(F →ₐ[fixed_points G F] F) ≤ dim F (F →ₗ[fixed_points G F] F) = dim (fixed_points G F) F
--/
-
-#check fintype_card_le_findim_of_linear_independent
-#check cardinal_mk_le_findim_of_linear_independent
+theorem dim_fixed_points (G : Type u) (F : Type v) [group G] [field F]
+  [fintype G] [faithful_mul_semiring_action G F] :
+  findim (fixed_points G F) F = fintype.card G :=
+le_antisymm (findim_fixed_points G F) $
+calc  fintype.card G
+    ≤ fintype.card (F →ₐ[fixed_points G F] F) :
+        fintype.card_le_of_injective _ (to_alg_hom_over_fixed_points G F).2
+... ≤ findim F (F →ₗ[fixed_points G F] F) : findim_alg_hom (fixed_points G F) F
+... = findim (fixed_points G F) F : findim_linear_map' _ _
